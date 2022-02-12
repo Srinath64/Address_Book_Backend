d77c1256461348510a7239e4091c3b80c35282ee
Srinath S <srinath@Srinaths-MacBook-Air.local>
2022-02-12 08:50:52 +0530

    Added URL for Postman to check the API's

--- src/main/java/com/bridgelabz/addressbook/controller/AddressBookController.java
+++ src/main/java/com/bridgelabz/addressbook/controller/AddressBookController.java
@@ -18,6 +18,7 @@ public class AddressBookController {
     @Autowired
     IAddressBookServices addressBookServices;
 
+    //http://localhost:8085/addressbook/details
     @GetMapping(value = {"", "/", "/details"})
     public ResponseEntity<ResponseDTO> getAddressBookData()
     {
@@ -26,6 +27,7 @@ public class AddressBookController {
         return new ResponseEntity<ResponseDTO>(respDTO, HttpStatus.OK);
     }
 
+    //http://localhost:8085/addressbook/details/1
     @GetMapping("/details/{personId}")
     public ResponseEntity<ResponseDTO> getAddressBookData(@PathVariable("personId") int personId)
     {
@@ -34,6 +36,7 @@ public class AddressBookController {
         return new ResponseEntity<ResponseDTO> (respDTO, HttpStatus.OK);
     }
 
+    //http://localhost:8085/addressbook/create
     @PostMapping("/create")
     public ResponseEntity<ResponseDTO> addAddressBookData(@RequestBody AddressBookDTO addressBookDTO)
     {
@@ -42,20 +45,22 @@ public class AddressBookController {
         return new ResponseEntity<ResponseDTO> (respDTO, HttpStatus.OK);
     }
 
+    //http://localhost:8085/addressbook/update/1
     @PutMapping("/update/{personId}")
     public ResponseEntity<ResponseDTO> updateAddressBookData(@PathVariable("personId") int personId,
                                                              @RequestBody AddressBookDTO addressBookDTO)
     {
         AddressBookData addressBookData = addressBookServices.updateAddressBookData(personId, addressBookDTO);
-        ResponseDTO respDTO = new ResponseDTO("updated adressBook data", addressBookDTO);
+        ResponseDTO respDTO = new ResponseDTO("Updated addressBook data", addressBookDTO);
         return new ResponseEntity<ResponseDTO> (respDTO, HttpStatus.OK);
     }
 
+    //http://localhost:8085/addressbook/delete/1
     @DeleteMapping("/delete/{personId}")
     public ResponseEntity<ResponseDTO> deleteAddressBookData(@PathVariable("personId") int personId)
     {
         addressBookServices.deleteAddressBookData(personId);
-        ResponseDTO respDTO = new ResponseDTO("deleted adressBook data with personId :", personId);
+        ResponseDTO respDTO = new ResponseDTO("Deleted addressBook data with personId :", personId);
         return new ResponseEntity<ResponseDTO> (respDTO, HttpStatus.OK);
     }
 
