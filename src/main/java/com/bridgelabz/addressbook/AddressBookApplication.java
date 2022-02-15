cb2de91698929c40540f9cf0e68d60510c0b53b9
Srinath S <srinath@Srinaths-MacBook-Air.local>
2022-02-15 11:01:36 +0530

    Added : Swagger for API Documentation

--- src/main/java/com/bridgelabz/addressbook/AddressBookApplication.java
+++ src/main/java/com/bridgelabz/addressbook/AddressBookApplication.java
@@ -4,15 +4,30 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @Slf4j
+@EnableSwagger2
 public class AddressBookApplication {
 
 	public static void main(String[] args) {
 		ApplicationContext context = SpringApplication.run(AddressBookApplication.class, args);
 		log.info("AddressBook App Started in {}", context.getEnvironment().getProperty("environment"));
 		log.info("AddressBook App Database User is {}", context.getEnvironment().getProperty("spring.datasource.username"));
+	}
+
+	@Bean
+	public Docket productApi() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.basePackage("com.bridgelabz.addressbook"))
+				.build();
 	}
 
 }
